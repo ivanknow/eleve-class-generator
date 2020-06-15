02f8040fa1639d4374ce77c011087a6167e6e9a7
Ivan Rodrigues <irodrigues@avenuecode.com>
2020-06-15 16:33:23 -0300

    refactoring proccess

--- backend/gerador/src/Conversor/AbstractConverter.php
+++ backend/gerador/src/Conversor/AbstractConverter.php
@@ -6,12 +6,7 @@ abstract class AbstractConverter {
 
     public $payload;
     
-    abstract public function generateAttr($payload);
-    abstract public function generateConstructor($payload);
-    abstract public function generateGetSet($payload);
-    abstract public function generateEquals($payload);
-    abstract public function generateToString($payload);
-    abstract public function generateMethod($payload);
-    abstract public function generateClass($payload);
+    abstract public function proccess($payload);
+    
 
 }
